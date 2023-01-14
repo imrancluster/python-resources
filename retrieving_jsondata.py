0df2959735a7a1aa30769a2db3bdcc94298105d8
Imran <imrancluster@gmail.com>
2023-01-14 23:58:53 +0600

    added example of  retrieving json data from the internet using basic oop

--- retrieving_jsondata.py
+++ retrieving_jsondata.py
@@ -6,6 +6,35 @@ from urllib.request import urlopen
 import json
 
 
+class Entity:
+    def __init__(self, api: str, description: str, link: str, category: str):
+        self._api = api
+        self._description = description
+        self._link = link
+        self._category = category
+
+    def __str__(self):
+        return "Entity({0}-{1})".format(self._api, self._category)
+
+
+def exract_result_by_oop(data):
+    json_data = json.loads(data)
+    entity_list = []
+
+    if "entries" in json_data:
+        for entity in json_data['entries'][:10]:
+            entity = Entity(
+                entity['API'],
+                entity['Description'],
+                entity['Link'],
+                entity['Category'],
+            )
+            entity_list.append(entity)
+
+    for entity in entity_list:
+        print(entity)
+
+
 def extract_result(data):
     json_data = json.loads(data)
 
@@ -22,7 +51,8 @@ def main():
     response = urlopen(rest_api_url)
     if response.getcode() == 200:
         data = response.read()
-        extract_result(data)
+        # extract_result(data)
+        exract_result_by_oop(data)
     else:
         print("getting error during calling the rest api: " + str(response.getcode()))
 
