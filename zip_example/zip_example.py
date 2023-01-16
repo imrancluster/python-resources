1e9d8f40b9f7c454c04c1c9f34d656d45e80e2e2
Imran <imrancluster@gmail.com>
2023-01-16 10:42:20 +0600

    added zip example with folder

--- zip_example/zip_example.py
+++ zip_example/zip_example.py
@@ -3,7 +3,16 @@
 #
 
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file),
+                       os.path.relpath(os.path.join(root, file),
+                                       os.path.join(path, '..')))
 
 
 def main():
@@ -12,6 +21,10 @@ def main():
     with ZipFile('new-folder.zip', 'w') as newzip:
         newzip.write('zip_testing_file2.txt')
         newzip.write('zip_testing_file1.txt')
+
+    # Zip example for folders
+    with ZipFile('Python.zip', 'w', ZIP_DEFLATED) as zipf:
+        zipdir('test_folder', zipf)
 
 
 if __name__ == '__main__':
